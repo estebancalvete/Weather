012c355514ed335bcc34859e3cc17bec2014ae3c
Esteban Calvete <esteban.calvete@gmail.com>
2021-04-14 20:25:46 +0100

    Created class NetworkService to get weather info from API

---
+++ Weather/Services/NetworkService.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkService.swift
+//  Weather
+//
+//  Created by Esteban Calvete Iglesias on 14/04/2021.
+//
+
+import Foundation
+
+class networkService {
+    
+    //MARK: Variables
+
+    var latitude: String = "52.5200"
+    var longitude: String = "13.4050"
+    let apiKey: String = "7564695d8d9f46060ac0234173fa0ea0"
+    let urlString: String = "https://api.openweathermap.org/data/2.5/onecall?lat=%@&lon=%@&appid=%@"
+    let session = URLSession(configuration: .default)
+
+    //var urlString: String = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitudeAsString)&lon=\(longitudeAsString)&appid=\(apiKey)"
+
+    //MARK: Functions
+
+    static let shared = networkService()
+
+
+    func getWeather() {
+        if let url = configureUrl() {
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print("Error")
+                    return
+                }
+                if let data = data {
+                    if let jsonString = String(data: data, encoding: .utf8) {
+                        print(jsonString)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
+    //MARK: Private
+
+    private func configureUrl () -> URL? {
+        let result: String = String(format: urlString, latitude, longitude, apiKey)
+        return URL(string: result)
+    }
+}
