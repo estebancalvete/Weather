eb765e663ba524a1b5020a6b1a1e472a3fd25bbc
Esteban Calvete <esteban.calvete@gmail.com>
2021-04-15 18:54:19 +0100

    Oultets added and storyboard updates

--- Weather/View/ViewController.swift
+++ Weather/View/ViewController.swift
@@ -9,12 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Variables
+    
+    
+    
+    //MARK: Constants
+    
+    
+    
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var cityNameLable: UILabel!
+    @IBOutlet weak var weatherTitleLable: UILabel!
+
+    @IBOutlet weak var weekDay1Lable: UILabel!
+    @IBOutlet weak var weekDay2Lable: UILabel!
+    @IBOutlet weak var weekDay3Lable: UILabel!
+    @IBOutlet weak var weekDay4Lable: UILabel!
+    @IBOutlet weak var weekDay5Lable: UILabel!
+    @IBOutlet weak var weekDay6Lable: UILabel!
+    
+    // Temperature Lables
+    
+    @IBOutlet weak var currentTempLable: UILabel!
+    @IBOutlet weak var day1TempLable: UILabel!
+    @IBOutlet weak var day2TempLable: UILabel!
+    @IBOutlet weak var day3TempLable: UILabel!
+    @IBOutlet weak var day4TempLable: UILabel!
+    @IBOutlet weak var day5TempLable: UILabel!
+    @IBOutlet weak var day6TempLable: UILabel!
+    
+    
+    // Weather Images
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var day1WeatherImage: UIImageView!
+    @IBOutlet weak var day2WeatherImage: UIImageView!
+    @IBOutlet weak var day3WeatherImage: UIImageView!
+    @IBOutlet weak var day4WeatherImage: UIImageView!
+    @IBOutlet weak var day5WeatherImage: UIImageView!
+    @IBOutlet weak var day6WeatherImage: UIImageView!
+    
+    
+    
+    //MARK: Live Cicle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
         networkService.shared.getWeather()
     }
 
+    //MARK: Functions
 
 }
 
